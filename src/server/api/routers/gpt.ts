27b0e8e83ef3e17469eae080b828a0b14dd25382
chiho13 <ahomnky11@gmail.com>
2023-03-28 00:55:20 +0100

    gpt 3.5

--- src/server/api/routers/gpt.ts
+++ src/server/api/routers/gpt.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { TRPCError } from "@trpc/server";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 export const GPTRouter = createTRPCRouter({
   getEquation: protectedProcedure
@@ -12,37 +18,28 @@ export const GPTRouter = createTRPCRouter({
     .query(async ({ input }) => {
       const { equationName } = input;
       try {
-        const response = await fetch(
-          "https://api.openai.com/v1/engines/text-davinci-003/completions",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        const completion = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: `You are a text to katex api.
+            Please return the JSON object containing the KaTeX code and explanation for '${equationName}', search for the concept relating to physics,
+             chemistry, mathematics, economics, finance. The JSON format should be {"result": "katex code", "note": "explanation"}. 
+             Preserve the single backslashes in the KaTeX code by using double backslashes in string.
+             Do not include any $ signs in the KaTeX code. 
+             Double check sources for accuracy. 
+             Be flexible with the input phrasing and recognize concepts even if they are slightly differently written.
+              If you don't recognize the input, return the json object {"error": "Input not recognised"}.
+             You must only give the JSON object in the response so it can be parsed.
+              `,
             },
-            body: JSON.stringify({
-              prompt: `
-              You are a text to katex api.
-              Please return the JSON object containing the KaTeX code and explanation for '${equationName}', search for the concept relating to physics,
-               chemistry, mathematics, economics, finance. The JSON format should be {"result": "katex code", "note": "explanation"}. 
-               Preserve the single backslashes in the KaTeX code by using double backslashes in string.
-               Do not include any $ signs in the KaTeX code. 
-               Double check sources for accuracy. 
-               Be flexible with the input phrasing and recognize concepts even if they are slightly differently written.
-                If you don't recognize the input, return the json object {"error": "Input not recognised"}.
-               You must only give the JSON object in the response so it can be parsed.
-                `,
+          ],
+          temperature: 0.6,
+        });
 
-              max_tokens: 400,
-              temperature: 0.2,
-            }),
-          }
-        );
-        const data = await response.json();
-
-        const jsonString = data.choices[0].text;
-
-        return jsonString;
+        const data = completion.data.choices[0].message.content;
+        return data;
       } catch (err) {
         console.error(err);
         throw new TRPCError({

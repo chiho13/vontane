66354ee20e579eabb80bc0f71368c2caa8df6356
chiho13 <ahomnky11@gmail.com>
2023-04-12 20:16:21 +0100

    bug fix and add option menu

--- src/components/DocumentEditor/EditorElements/MCQElement.tsx
+++ src/components/DocumentEditor/EditorElements/MCQElement.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ReactEditor } from "slate-react";
+import { EditorContext } from "@/contexts/EditorContext";
 
 interface MCQElementProps {
   attributes: any;
   children: React.ReactNode;
+  element: any;
 }
 
 export const MCQElement: React.FC<MCQElementProps> = ({
+  element,
   attributes,
   children,
 }) => {
+  const { editor } = useContext(EditorContext);
+  const path = ReactEditor.findPath(editor, element);
   return (
-    <div {...attributes} className="mcq-element rounded-md bg-gray-100 p-4">
+    <div
+      {...attributes}
+      className="mcq-element rounded-md bg-gray-100 p-4"
+      data-id={element.id}
+      data-path={JSON.stringify(path)}
+    >
       {children}
     </div>
   );

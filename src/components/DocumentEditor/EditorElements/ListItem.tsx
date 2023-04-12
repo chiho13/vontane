5429a012625fbb22322ba1af1fc2d2d3a2273d0e
chiho13 <ahomnky11@gmail.com>
2023-04-12 04:26:57 +0100

    Set solution

--- src/components/DocumentEditor/EditorElements/ListItem.tsx
+++ src/components/DocumentEditor/EditorElements/ListItem.tsx
@@ -1,25 +1,49 @@
 import React from "react";
+import { Editor, Transforms, createEditor, Path } from "slate";
+import { useContext } from "react";
+import { EditorContext } from "@/contexts/EditorContext";
+import { ReactEditor } from "slate-react";
 
-interface ListItemProps {
-  attributes: any;
-  children: React.ReactNode;
-  correctAnswer: boolean;
-}
+// Custom List Item component
+export const ListItem = ({ attributes, children, element }) => {
+  const { editor } = useContext(EditorContext);
 
-export const ListItem: React.FC<ListItemProps> = ({
-  element,
-  attributes,
-  children,
-}) => {
-  const [selected, setSelected] = React.useState(element.correctAnswer);
+  const handleChange = (e) => {
+    const path = ReactEditor.findPath(editor, element);
+    const isSelected = e.target.checked;
 
-  const handleRadioChange = () => {
-    setSelected(!selected);
+    // Unset correctAnswer for all other siblings if the current option is being set to true
+    if (isSelected) {
+      const parentPath = Path.parent(path);
+      const [parentNode] = Editor.node(editor, parentPath);
+
+      parentNode.children.forEach((child, index) => {
+        if (child.correctAnswer) {
+          Transforms.setNodes(
+            editor,
+            { correctAnswer: false },
+            { at: [...parentPath, index] }
+          );
+        }
+      });
+    }
+
+    // Set correctAnswer for the current option
+    Transforms.setNodes(editor, { correctAnswer: isSelected }, { at: path });
   };
+
   return (
     <li {...attributes} className="ml-10">
-      {children}
-      {/* {element.correctAnswer && <span className="text-green-500">✔</span>} */}
+      <div className="flex items-center">
+        {children}
+        <input
+          type="checkbox"
+          id={element.id}
+          className="ml-2 h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+          checked={element.correctAnswer || false}
+          onChange={handleChange}
+        />
+      </div>
     </li>
   );
 };

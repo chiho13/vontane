cd3c5f5a9227ff4ca2fb48059a2b84298637ce11
chiho13 <ahomnky11@gmail.com>
2023-04-27 16:19:38 +0100

    refactor before slide integration

---
+++ src/components/DocumentEditor/helpers/addMCQBlock.ts
@@ -0,0 +1,68 @@
+// src/helpers/mcqHelper.js
+import { Editor, Path, Transforms } from "slate";
+import { ReactEditor } from "slate-react";
+import { genNodeId } from "@/hoc/withID";
+
+export const addMCQBlock = (editor, path) => {
+  const mcqNode = {
+    id: genNodeId(),
+    type: "mcq",
+    children: [
+      {
+        id: genNodeId(),
+        type: "list-item",
+        children: [
+          {
+            text: "",
+          },
+        ],
+      },
+      {
+        id: genNodeId(),
+        type: "ol",
+        children: [
+          {
+            id: genNodeId(),
+            type: "option-list-item",
+            children: [
+              {
+                text: "",
+              },
+            ],
+            correctAnswer: false,
+          },
+          {
+            id: genNodeId(),
+            type: "option-list-item",
+            children: [
+              {
+                text: "",
+              },
+            ],
+            correctAnswer: true,
+          },
+        ],
+      },
+    ],
+  };
+
+  const [currentNode] = Editor.node(editor, path);
+  const isEmptyNode =
+    currentNode.type === "paragraph" &&
+    currentNode.children.length === 1 &&
+    currentNode.children[0].text === "";
+
+  let newPath;
+  if (isEmptyNode) {
+    Transforms.insertNodes(editor, mcqNode, { at: path });
+    newPath = path;
+  } else {
+    Transforms.insertNodes(editor, mcqNode, { at: Path.next(path) });
+    newPath = Path.next(path);
+  }
+
+  const listItemPath = newPath.concat([0, 0]);
+  const listItemPoint = { path: listItemPath, offset: 0 };
+  Transforms.select(editor, listItemPoint);
+  ReactEditor.focus(editor);
+};

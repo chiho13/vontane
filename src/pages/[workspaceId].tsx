41fc3868c842d6b3e6066b78df05f349967555e9
chiho13 <ahomnky11@gmail.com>
2023-04-29 18:22:49 +0100

    replace

--- src/pages/[workspaceId].tsx
+++ src/pages/[workspaceId].tsx
@@ -1,25 +1,23 @@
-import {
-  GetStaticPropsContext,
-  type NextPage,
-  InferGetStaticPropsType,
-  GetStaticPaths,
-} from "next";
-
 import React, { useState, useEffect, useRef, useCallback } from "react";
-
 import { useSession } from "@supabase/auth-helpers-react";
 import LoginPage from "./login";
 import { useRouter } from "next/router";
 import { WorkspaceContainer } from "@/components/WorkspaceContainer";
 import { useUserContext } from "@/contexts/UserContext";
 import Layout from "@/components/Layouts/AccountLayout";
-// import "react-mirt/dist/css/react-mirt.css";
+import { NextPage } from "next";
 
 const Workspace: NextPage = () => {
   const session = useSession();
   const router = useRouter();
   const { profile } = useUserContext();
   const workspaceId = router.query.workspaceId;
+
+  useEffect(() => {
+    if (!session) {
+      router.replace("/login");
+    }
+  }, [session, router]);
 
   return session ? (
     <Layout profile={profile} currentWorkspaceId={workspaceId}>

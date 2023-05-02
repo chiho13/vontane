b624d810959b12dcf0495eea3eb0dce773d20f79
chiho13 <ahomnky11@gmail.com>
2023-05-02 16:51:33 +0100

    Account page

---
+++ src/components/AccountSettingsNav/index.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { SidebarNavItem } from "@/types";
+import { cn } from "@/utils/cn";
+import { Icons } from "@/components/Icons";
+
+interface DashboardNavProps {
+  items: SidebarNavItem[];
+}
+
+export function DashboardNav({ items }: DashboardNavProps) {
+  const path = usePathname();
+
+  if (!items?.length) {
+    return null;
+  }
+
+  return (
+    <nav className="grid items-start gap-2">
+      {items.map((item, index) => {
+        const Icon = Icons[item.icon || "arrowRight"];
+        return (
+          item.href && (
+            <Link key={index} href={item.disabled ? "/" : item.href}>
+              <span
+                className={cn(
+                  "group flex items-center rounded-md px-3 py-2 text-sm  font-medium text-gray-600 transition duration-300 hover:bg-gray-200 hover:text-black",
+                  path === item.href ? "bg-gray-200 text-black" : "transparent",
+                  item.disabled && "cursor-not-allowed opacity-80"
+                )}
+              >
+                <Icon className="mr-2 h-4 w-4" />
+                <span>{item.title}</span>
+              </span>
+            </Link>
+          )
+        );
+      })}
+    </nav>
+  );
+}
---
+++ src/types/index.d.ts
@@ -0,0 +1,15 @@
+export type SidebarNavItem = {
+  title: string;
+  disabled?: boolean;
+  external?: boolean;
+  icon?: keyof typeof Icons;
+} & (
+  | {
+      href: string;
+      items?: never;
+    }
+  | {
+      href?: string;
+      items: NavLink[];
+    }
+);

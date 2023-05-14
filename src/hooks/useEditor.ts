ffc8f84626073ee6b360ad29520d3dea0f5f6ba0
chiho13 <ahomnky11@gmail.com>
2023-05-14 13:19:48 +0100

    editor skeleton

--- src/hooks/useEditor.ts
+++ src/hooks/useEditor.ts
@@ -41,27 +41,6 @@ const withNormalizePasting = (editor) => {
   return editor;
 };
 
-const wrapLink = (editor, url: string) => {
-  // if (isLinkActive(editor)) {
-  //   unwrapLink(editor)
-  // }
-
-  const { selection } = editor;
-  const isCollapsed = selection && Range.isCollapsed(selection);
-  const link = {
-    type: "link",
-    url,
-    children: isCollapsed ? [{ text: url }] : [],
-  };
-
-  if (isCollapsed) {
-    Transforms.insertNodes(editor, link);
-  } else {
-    Transforms.wrapNodes(editor, link, { split: true });
-    Transforms.collapse(editor, { edge: "end" });
-  }
-};
-
 export function useEditor() {
   const editor = useMemo(
     () =>

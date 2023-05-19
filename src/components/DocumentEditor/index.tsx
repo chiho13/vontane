ae0b406e31ccea6b0a22397228285253e62f4f17
chiho13 <ahomnky11@gmail.com>
2023-05-19 01:24:42 +0100

    link inline

--- src/components/DocumentEditor/index.tsx
+++ src/components/DocumentEditor/index.tsx
@@ -1061,12 +1061,12 @@ export const DocumentEditor: React.FC<DocumentEditorProps> = ({
       );
 
       return (
-        <div>
+        <>
           {content}
           {/* <div className="invisible opacity-0 transition-all duration-300 group-hover:visible group-hover:opacity-100">
             {optionMenu}
           </div>  */}
-        </div>
+        </>
       );
     },
     []

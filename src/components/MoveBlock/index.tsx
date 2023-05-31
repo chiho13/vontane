9253b96f6b25d575ed8fc09c62b55730238f6c6e
chiho13 <ahomnky11@gmail.com>
2023-05-31 15:44:21 +0100

    move block up and down tts

---
+++ src/components/MoveBlock/index.tsx
@@ -0,0 +1,98 @@
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { Editor, Path, Transforms } from "slate";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface MoveBlockProps {
+  editor: Editor;
+  path: Path;
+}
+
+export const MoveBlock: React.FC<MoveBlockProps> = ({ editor, path }) => {
+  const moveBlock = (direction: "up" | "down") => {
+    const currentPath = path;
+    const previousPath = Path.previous(currentPath);
+    const nextPath = Path.next(currentPath);
+
+    let newPath;
+    if (direction === "up") {
+      newPath = previousPath;
+    } else if (direction === "down") {
+      newPath = nextPath;
+    }
+
+    if (newPath) {
+      Transforms.moveNodes(editor, {
+        at: currentPath,
+        to: newPath,
+      });
+    }
+  };
+
+  const selectedBlockIndex = path[0];
+  return (
+    <div className="absolute  top-[7px] left-[10px] flex flex-col">
+      {selectedBlockIndex && selectedBlockIndex > 1 && (
+        <TooltipProvider delayDuration={0}>
+          <Tooltip>
+            <TooltipTrigger>
+              <button
+                className="addButton flex h-[22px] w-[22px] items-center justify-center rounded-md ease-in-out hover:bg-gray-200 dark:hover:bg-accent"
+                onClick={(event) => {
+                  event.preventDefault();
+                  event.stopPropagation();
+                  moveBlock("up");
+                }}
+              >
+                <ChevronUp
+                  className="stroke-darkgray dark:stroke-muted-foreground"
+                  width={20}
+                />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent
+              className="border-black  dark:bg-white dark:text-muted"
+              side="left"
+              sideOffset={10}
+            >
+              <p className="text-[12px]">Move up</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      )}
+      {selectedBlockIndex &&
+        selectedBlockIndex < editor.children.length - 1 && (
+          <TooltipProvider delayDuration={0}>
+            <Tooltip>
+              <TooltipTrigger>
+                <button
+                  className="addButton flex h-[22px] w-[22px] items-center justify-center rounded-md ease-in-out hover:bg-gray-200 dark:hover:bg-accent"
+                  onClick={(event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    moveBlock("down");
+                  }}
+                >
+                  <ChevronDown
+                    className="stroke-darkgray dark:stroke-muted-foreground"
+                    width={20}
+                  />
+                </button>
+              </TooltipTrigger>
+              <TooltipContent
+                className="border-black  dark:bg-white dark:text-muted"
+                side="left"
+                sideOffset={10}
+              >
+                <p className="text-[12px]">Move down</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
+    </div>
+  );
+};

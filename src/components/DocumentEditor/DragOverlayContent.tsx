54bccfdd930a07fa163dcbf10151a1be43ea99b3
Anthony Ho <ahomnky11@gmail.com>
2023-07-06 19:20:00 +0100

    drag overlay

--- src/components/DocumentEditor/DragOverlayContent.tsx
+++ src/components/DocumentEditor/DragOverlayContent.tsx
@@ -1,4 +1,5 @@
 import { useEditor } from "@/hooks/useEditor";
+import React from "react";
 import { useContext, useState } from "react";
 import { Editable, Slate } from "slate-react";
 import { LayoutContext } from "../Layouts/AccountLayout";

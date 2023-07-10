42bde6fd21e6c75444cc94bdefd2eaaeeb2c2ece
Anthony Ho <ahomnky11@gmail.com>
2023-07-10 16:49:30 +0100

    es lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,14 @@
-// module.exports = {
-//   env: {
-//     browser: true,
-//     es2021: true,
-//   },
-//   extends: [
-//     'plugin:react/recommended',
-//     'plugin:@typescript-eslint/recommended',
-//     'prettier',
-//   ],
-//   parser: '@typescript-eslint/parser',
-//   parserOptions: {
-//     ecmaFeatures: {
-//       jsx: true,
-//     },
-//     ecmaVersion: 12,
-//     sourceType: 'module',
-//   },
-//   plugins: [
-//     'react',
-//     '@typescript-eslint',
-//   ],
-//   rules: {
-//     "react/display-name": "off",
-//   },
-// };
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+};

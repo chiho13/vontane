51a488d265dfa31ee1994f103c712c65b01eb1bd
Anthony Ho <ahomnky11@gmail.com>
2023-07-17 17:39:44 +0100

    embed iframe

---
+++ src/hooks/useClipboard.ts
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const useClipboard = () => {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
+  return { copied, copyToClipboard };
+};

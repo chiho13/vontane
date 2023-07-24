4d64649eefbe17a224c9ae57c0f1b3941b844aa8
Anthony Ho <ahomnky11@gmail.com>
2023-07-24 22:39:34 +0100

    web socket ws

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,35 @@ import { api } from "@/utils/api";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { UserContextProvider } from "@/contexts/UserContext";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { WorkspaceTitleUpdateProvider } from "@/contexts/WorkspaceTitleContext";
+import { createTRPCProxyClient, createWSClient, wsLink } from "@trpc/client";
+import type { AppRouter } from "@/server/api/root"; // replace with your actual path
+
 import "@/styles/globals.css";
 import "@stripe/stripe-js";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  // create persistent WebSocket connection
+  const wsClient = useMemo(() => {
+    return createWSClient({
+      url: `ws://localhost:3001`,
+    });
+  }, []);
+
+  // configure TRPCClient to use WebSockets transport
+  const client = useMemo(() => {
+    return createTRPCProxyClient({
+      links: [
+        wsLink({
+          client: wsClient,
+        }),
+      ],
+    });
+  }, [wsClient]);
 
   return (
     <SessionContextProvider supabaseClient={supabaseClient}>

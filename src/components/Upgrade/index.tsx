f8ad9232895e0ff02700717e8b6f4a3186f17443
Anthony Ho <ahomnky11@gmail.com>
2023-07-24 01:30:26 +0100

    checkout wip

--- src/components/Upgrade/index.tsx
+++ src/components/Upgrade/index.tsx
@@ -59,28 +59,17 @@ const Upgrade = () => {
   const theme = useTheme();
   const router = useRouter();
   const session = useSession();
-  const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState(null);
 
   const [toggle, setToggle] = useState(true);
   const toggleClass = " transform translate-x-[22px]";
-  const queryResult = api.checkout.fetchProducts.useQuery();
-  const { data } = queryResult;
+  //   const queryResult = api.checkout.fetchProducts.useQuery();
+  //   const { data } = queryResult;
 
-  const prices = data && data[0].prices;
+  //   const prices = data && data;
 
-  console.log(prices);
   const [selectedPrice, setSelectedPrice] = useState("");
   const { profile, isLoading }: any = useUserContext();
-
-  useEffect(() => {
-    if (prices) {
-      if (toggle) {
-        setSelectedPrice(prices[1].id);
-      } else {
-        setSelectedPrice(prices[0].id);
-      }
-    }
-  }, [toggle]);
 
   useEffect(() => {
     if (profile) {
@@ -88,20 +77,15 @@ const Upgrade = () => {
     }
   }, [profile]);
 
-  useEffect(() => {
-    if (prices) {
-      setSelectedPrice(prices[1].id);
-      console.log(prices[1]);
-    }
-  }, [prices]);
-
   const createCheckoutSessionMutation =
     api.checkout.createCheckoutSession.useMutation();
 
   const createCheckoutSession = async (_price: string) => {
     if (loading) return;
     try {
-      setLoading(true);
+      setLoading({
+        [_price]: true,
+      });
       const { checkoutUrl } = await createCheckoutSessionMutation.mutateAsync({
         price: _price,
       });
@@ -109,10 +93,10 @@ const Upgrade = () => {
       if (checkoutUrl) {
         void router.push(checkoutUrl);
       }
-      setLoading(false);
+      setLoading({});
       // stripe?.redirectToCheckout({ sessionId });
     } catch (error) {
-      setLoading(false);
+      setLoading({});
       console.error("Error creating checkout session:", error);
     }
   };
@@ -130,6 +114,88 @@ const Upgrade = () => {
   };
 
   console.log(profile?.is_subscribed);
+  const PriceCards = [
+    {
+      credits: "25,000 credits",
+      price: "9",
+      audioTime: "~ 30 minutes of Audio",
+      checkoutId: "price_1NX6HtGj8eKfeqgl2PoJ7aOd",
+    },
+    {
+      credits: "7-day Pass",
+      price: "39",
+      audioTime: "Unlimited",
+      checkoutId: "price_1NX6kZGj8eKfeqglc8U3gFRc",
+    },
+    {
+      credits: "30-day Pass",
+      price: "99",
+      audioTime: "Unlimited",
+      checkoutId: "price_1NX6kzGj8eKfeqglERCZRSZf",
+      extra: "Priority Support",
+    },
+  ];
+
+  const PriceCard = ({
+    credits,
+    price,
+    audioTime,
+    checkoutId,
+    extra,
+    loading,
+    createCheckoutSession,
+  }) => {
+    return (
+      <div className="relative z-10 flex flex-col rounded-xl  border border-gray-200 bg-white px-8 py-10 shadow-md  dark:bg-secondary sm:p-12 lg:grid-cols-2 lg:px-6 lg:py-10 xl:p-10">
+        <div className="relative grow ">
+          <span className="mb-2 block  text-[28px] text-lg font-semibold text-[#0E78EF] dark:text-foreground">
+            {credits}
+          </span>
+
+          <div className="relative">
+            <h2 className=" relative mb-2 mt-3  flex text-[40px] font-bold text-gray-500  dark:text-foreground  ">
+              £<span className="block text-foreground">{price}</span>
+            </h2>
+          </div>
+          <div className="relative mb-7 block">
+            <p className="mt-3  flex items-center text-left text-base font-semibold leading-loose text-gray-500 dark:text-gray-400">
+              <Check />
+              <span className="shrink pl-2 text-brand">{audioTime}</span>
+            </p>
+            <p className="mt-2  flex items-center text-left font-semibold leading-loose text-gray-500  dark:text-gray-400">
+              <Check />
+              <span className="pl-2">One time payment</span>
+            </p>
+
+            <p className="mt-3 flex items-center text-left  font-semibold  leading-loose text-gray-500 dark:text-gray-400">
+              <Check />
+              <span className="pl-2">No subscription</span>
+            </p>
+            {extra && (
+              <p className="mt-3 flex items-center text-left  font-semibold  leading-loose text-gray-500 dark:text-gray-400">
+                <Check />
+                <span className="pl-2">{extra}</span>
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="relative h-[50px]">
+          <button
+            onClick={() => createCheckoutSession(checkoutId)}
+            className="subscribe-btn flex w-full items-center justify-center rounded-md border p-2 text-center text-base font-semibold text-white transition"
+          >
+            <div className="flex items-center justify-center">
+              {loading ? (
+                <LoadingSpinner strokeColor="stroke-white" />
+              ) : (
+                <span className="ml-2 text-lg">Buy</span>
+              )}
+            </div>
+          </button>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <PricingStyle>
@@ -145,146 +211,18 @@ const Upgrade = () => {
         </div>
         <div className="relative -mx-4 mb-10 flex justify-center px-10">
           <div className="grid w-full grid-cols-3 gap-2">
-            <div className="relative z-10 flex flex-col rounded-xl  border border-gray-200 bg-white px-8 py-10 shadow-md  dark:bg-secondary sm:p-12 lg:grid-cols-2 lg:px-6 lg:py-10 xl:p-10">
-              <div className="relative grow ">
-                <span className="mb-2 block  text-[28px] text-lg font-semibold text-[#0E78EF] dark:text-foreground">
-                  25,000 credits
-                </span>
-
-                <div className="relative">
-                  <h2 className=" relative mb-2 mt-3  flex text-[40px] font-bold text-gray-500  dark:text-foreground  ">
-                    £<span className="block text-foreground">9</span>
-                  </h2>
-                </div>
-                <div className="relative mb-7 block">
-                  <p className="mt-3  flex items-center text-left text-base font-semibold leading-loose text-gray-500 dark:text-gray-400">
-                    <Check />
-                    <span className="pl-2">{"~ 30 minutes of Audio"}</span>
-                  </p>
-                  <p className="mt-2  flex items-center text-left font-semibold leading-loose text-gray-500  dark:text-gray-400">
-                    <Check />
-                    <span className="pl-2">One time payment</span>
-                  </p>
-
-                  <p className="mt-3 flex items-center text-left  font-semibold  leading-loose text-gray-500 dark:text-gray-400">
-                    <Check />
-                    <span className="pl-2">No subscription</span>
-                  </p>
-                </div>
-              </div>
-              <div className="relative h-[50px]">
-                <button
-                  onClick={() => createCheckoutSession(selectedPrice)}
-                  className="subscribe-btn flex w-full items-center justify-center rounded-md border p-2 text-center text-base font-semibold text-white transition"
-                >
-                  <div className="flex items-center justify-center">
-                    {loading ? (
-                      <LoadingSpinner strokeColor="stroke-white" />
-                    ) : (
-                      <span className="ml-2 text-lg">Buy</span>
-                    )}
-                  </div>
-                </button>
-              </div>
-            </div>
-            <div className="relative z-10 flex flex-col rounded-xl  border border-gray-200 bg-white px-8 py-10 shadow-md  dark:bg-secondary sm:p-12 lg:grid-cols-2 lg:px-6 lg:py-10 xl:p-10">
-              <div className="relative grow ">
-                <span className="mb-2 block  text-[28px] text-lg font-semibold text-[#0E78EF] dark:text-foreground">
-                  7-day Pass{" "}
-                </span>
-
-                <div className="relative">
-                  <h2 className=" relative mb-2 mt-3  flex text-[40px] font-bold text-gray-500  dark:text-foreground  ">
-                    £<span className="block text-foreground">39</span>
-                  </h2>
-                </div>
-                <div className="relative mb-7 block">
-                  <p className="mt-3  flex items-center text-left text-base font-semibold leading-loose text-gray-500 dark:text-gray-400">
-                    <Check />
-                    <span className="pl-2">
-                      <span className="text-[#0E78EF] dark:text-foreground">
-                        Unlimited
-                      </span>{" "}
-                      Usage and Revisions
-                    </span>
-                  </p>
-                  <p className="mt-2  flex items-center text-left font-semibold leading-loose text-gray-500  dark:text-gray-400">
-                    <Check />
-                    <span className="pl-2">One time payment</span>
-                  </p>
-
-                  <p className="mt-3 flex items-center text-left  font-semibold  leading-loose text-gray-500 dark:text-gray-400">
-                    <Check />
-                    <span className="pl-2">No subscription</span>
-                  </p>
-                </div>
-              </div>
-              <div className="relative h-[50px]">
-                <button
-                  onClick={() => createCheckoutSession(selectedPrice)}
-                  className="subscribe-btn flex w-full items-center justify-center rounded-md border p-2 text-center text-base font-semibold text-white transition"
-                >
-                  <div className="flex items-center justify-center">
-                    {loading ? (
-                      <LoadingSpinner strokeColor="stroke-white" />
-                    ) : (
-                      <span className="ml-2 text-lg">Buy</span>
-                    )}
-                  </div>
-                </button>
-              </div>
-            </div>
-            <div className="relative z-10 rounded-xl  border border-gray-200 bg-white px-8 py-10 shadow-md  dark:bg-secondary sm:p-12 lg:grid-cols-2 lg:px-6 lg:py-10 xl:p-10">
-              <div className="relative">
-                <span className="mb-2 block  text-[28px] text-lg font-semibold text-[#0E78EF] dark:text-foreground">
-                  30-day Pass{" "}
-                </span>
-
-                <div className="relative">
-                  <h2 className=" relative mb-2 mt-3  flex text-[40px] font-bold text-gray-500  dark:text-foreground  ">
-                    £<span className="block text-foreground">99</span>
-                  </h2>
-                </div>
-                <div className="relative mb-7 block">
-                  <p className="mt-3  flex items-center text-left text-base font-semibold leading-loose text-gray-500 dark:text-gray-400">
-                    <Check />
-                    <span className="pl-2">
-                      <span className="text-[#0E78EF] dark:text-foreground">
-                        Unlimited
-                      </span>{" "}
-                      Usage and Revisions
-                    </span>
-                  </p>
-                  <p className="mt-2  flex items-center text-left font-semibold leading-loose text-gray-500  dark:text-gray-400">
-                    <Check />
-                    <span className="pl-2">One time payment</span>
-                  </p>
-
-                  <p className="mt-3 flex items-center text-left  font-semibold  leading-loose text-gray-500 dark:text-gray-400">
-                    <Check />
-                    <span className="pl-2">No subscription</span>
-                  </p>
-                  <p className="mt-3 flex items-center text-left  font-semibold  leading-loose text-gray-500 dark:text-gray-400">
-                    <Check />
-                    <span className="pl-2">Priority Support</span>
-                  </p>
-                </div>
-              </div>
-              <div className="relative h-[50px]">
-                <button
-                  onClick={() => createCheckoutSession(selectedPrice)}
-                  className="subscribe-btn flex w-full items-center justify-center rounded-md border p-2 text-center text-base font-semibold text-white transition"
-                >
-                  <div className="flex items-center justify-center">
-                    {loading ? (
-                      <LoadingSpinner strokeColor="stroke-white" />
-                    ) : (
-                      <span className="ml-2 text-lg">Buy</span>
-                    )}
-                  </div>
-                </button>
-              </div>
-            </div>
+            {PriceCards.map((card, index) => (
+              <PriceCard
+                key={index}
+                credits={card.credits}
+                price={card.price}
+                audioTime={card.audioTime}
+                checkoutId={card.checkoutId}
+                extra={card.extra}
+                loading={loading}
+                createCheckoutSession={createCheckoutSession}
+              />
+            ))}
           </div>
         </div>
       </div>

dd99f19bec4d5e7ca8172f16f6b4ec7a86aec571
Anthony Ho <ahomnky11@gmail.com>
2023-08-11 21:52:13 +0100

    export

--- src/components/Export/index.tsx
+++ src/components/Export/index.tsx
@@ -1,4 +1,4 @@
-import { MoreHorizontal, Trash2, Copy } from "lucide-react";
+import { MoreHorizontal, Trash2, Copy, ChevronRight } from "lucide-react";
 // import { isParentTTS, wrapWithTTS } from "./helpers/toggleBlock";
 import { cn } from "@/utils/cn";
 import { exportToHTML } from "@/utils/htmlSerialiser";
@@ -72,18 +72,9 @@ export const Export = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <TooltipProvider delayDuration={300}>
-          <Tooltip>
-            <TooltipTrigger>
-              <Button className="flex h-[22px] w-[22px] items-center justify-center rounded-md bg-white p-0 outline-none hover:bg-gray-200 dark:bg-muted dark:hover:bg-accent">
-                <MoreHorizontal className="option-menu w-[18px] w-[18px] text-darkergray dark:text-foreground" />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="right" sideOffset={10}>
-              <p className="text-[12px]">Export</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <Button className="border" variant="outline" size="xs">
+          Export <ChevronRight className="w-4" />
+        </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className=" top-0 w-[200px] border dark:border-accent dark:bg-muted">
         <DropdownMenuItem

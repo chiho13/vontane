c852b99df4aabee5c916e66ee0b02dd3760297c3
Anthony Ho <ahomnky11@gmail.com>
2023-08-16 23:45:16 +0100

    remove padding

--- src/components/DocumentEditor/EditorElements/ColumnElement.tsx
+++ src/components/DocumentEditor/EditorElements/ColumnElement.tsx
@@ -12,7 +12,7 @@ export function ColumnElement(props: any) {
       {...attributes}
       data-id={element.id}
       data-path={JSON.stringify(path)}
-      className="grid w-full grid-cols-2 items-start  pb-2 pt-2"
+      className="grid w-full grid-cols-2 items-start"
     >
       {children}
     </div>

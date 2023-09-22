969f40ea7ae3bc0ff91192fd04b161c096b852c4
Anthony Ho <ahomnky11@gmail.com>
2023-09-23 00:34:27 +0100

    bg

--- src/pages/[publishedId].tsx
+++ src/pages/[publishedId].tsx
@@ -42,7 +42,7 @@ const PublishedPage = ({ workspaceData, font, brandColor }) => {
   const router = useRouter();
   const workspaceId = router.query.workspaceId as string;
   return (
-    <div className="h-[100vh] overflow-y-auto px-6 pt-[30px] xl:pt-[60px]">
+    <div className="h-[100vh] overflow-y-auto px-6 pt-[30px] dark:bg-[#191919] xl:pt-[60px]">
       <WidgetRenderer
         key={workspaceId}
         workspaceData={workspaceData}
